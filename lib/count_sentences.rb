bdd51bab943f186a103b835d15475cbc20e56498
verah <verahnyagaka101@gmail.com>
2022-08-17 17:41:38 +0300

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,31 @@
 require 'pry'
 
 class String
+attr_accessor :string
 
+  # def sentence?(string_end)
+  #   self.string = string_end
+  #   string_end.end_with?("!",".","?")  
+  # end
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/(?<=[.!?]).(?=[a-zA-Z])/).count
   end
 end
+
+man = String.new('This, well, is a sentence. This is too!! And so is this, I think? Woo...')
+puts man.sentence?
+puts man.count_sentences
+
+# binding.pry
