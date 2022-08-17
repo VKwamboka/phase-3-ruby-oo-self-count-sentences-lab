a1aa2456c7ad9126102a7f8c0f5f1b890853f5e4
verah <verahnyagaka101@gmail.com>
2022-08-17 17:36:53 +0300

    commit tree 88387b6253a21f418d6e2265ba3a270da7144446

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,31 @@
 require 'pry'
 
 class String
+attr_accessor :string
 
+  # def sentence?(string_end)
+  #   self.string = string_end
+  #   string_end.end_with?("!",".","?")  
+  # end
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/(?<=[.!?]).(?=[a-zA-Z])/).count
   end
 end
+
+man = String.new('hy.the.heck?haha!')
+puts man.sentence?
+puts man.count_sentences
+
+# binding.pry
